ae1ff14580fd3768c9953df637d0f764a41398db
Ben Stewart <benjamin.r.stewart95@gmail.com>
2022-11-09 23:23:13 -0700

    implementedall methods, sort is borked

--- Assignment1/stats.c
+++ Assignment1/stats.c
@@ -28,6 +28,8 @@
 /* Size of the Data Set */
 #define SIZE (40)
 
+
+
 void main() {
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
@@ -36,44 +38,97 @@ void main() {
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
+
+  uint _size = (uint)SIZE; 
+  printf("Initial Array:\r\n");
+  print_array(test, _size); //print inital array
+  sort_array(test,_size);
+  printf("Sorted Array:\r\n");
+  print_array(test, _size); //Sort it, print it again. Allow for visual verification of the data. 
+
+  //calculate statistical values
+  find_mean(test,_size);
+  find_median(test,_size);
+  find_maximum(test,_size);
+  find_minimum(test,_size);
+
+
+  print_statistics(test,_size); 
+
+  printf("complete\r\n"); 
+
+}
+
+void print_statistics()
+{
+  printf("Mean: %i\r\n",stats_mean);
+  printf("Median: %i\r\n",stats_mean);
+  printf("Minimum: %i\r\n",stats_minimum);
+  printf("Maximum: %i\r\n",stats_maximum);
+}
+
+void print_array (uint8 data[], uint length)
+{
+  for(int i = 0; i < length; i++)
+  {
+    printf("%i,",data[i]);
+  }     
+  printf("\r\n");
+}
+
+void sort_array(uint8 data[], uint length)
+{
+  
+    //just use a basic insertion sort, it's not the most efficient but it's easy to implement in c, and it runs fast when it's already sorted
+  uint8 current = data[0]; 
+  for(int i = length - 1; i >= 0; i--)
+  {
+    for(int j = i-1; j >= 0; j--)
+    { 
+      if(data[j] > data[j + 1])
+      {
+        current = data[j]; 
+        data[j] = data[j+1];
+        data[j+1] = current; 
+      }
+      else
+        break; 
+    }
+  }
+
+}
+
+uint8 find_mean(uint8 data[], uint length)
+{
+  uint total; 
+  for(int i = 0; i < length; i++)
+  {
+    total += (uint)data[i];
+  }  
+  stats_mean = total / length;   
+  return stats_mean; 
+}
+
+//Contract doesn't state if we can assume that the array is presorted, so sort first then find the values.
+uint8 find_median(uint8 data[], uint length)
+{
+  sort_array(data,length); 
+  stats_median = data[length/2]; 
+  return stats_median; 
+}
+
+uint8 find_maximum(uint8 data[], uint length)
+{
+  sort_array(data,length); 
+  stats_maximum = data[0];
+  return stats_maximum; 
+}
+
  
-
-}
-
-void print_statistics(uint8 * data, uint length)
+uint8 find_minimum(uint8 data[], uint length)
 {
-    
+  sort_array(data,length); 
+  stats_minimum = data[length -1];
+  return stats_minimum;
 }
 
-void print_array (uint8 * data, uint length)
-{
-    
-}
-
- 
-uint8 find_median(uint8 * data, uint length)
-{
-
-}
-
- 
-uint8 find_mean(uint8 * data, uint length)
-{
-
-}
-
-uint8 find_maximum(uint8 * data, uint length)
-{
-
-}
-
- 
-uint8 find_minimum(uint8 * data, uint length)
-{
-
-}
-
-void sort_array(uint8 * data, uint length)
-{
-    
-}
--- Assignment1/stats.h
+++ Assignment1/stats.h
@@ -13,26 +13,32 @@
 
 ///declarations
 
+uint8 stats_mean;
+uint8 stats_median;
+uint8 stats_maximum;
+uint8 stats_minimum;
+
+
 //a function that prints the statistics of a data set, including minimum, maximum, mean, and median. 
-void print_statistics( uint8 * data, uint length); 
+void print_statistics(); 
 
 // given an array of a data and a length, print the array nicely. 
-void print_array(uint8 * data, uint length);
+void print_array(uint8 data[], uint length);
 
 
 // given an array of a data and a length, find the median of the array. 
-uint8 find_median(uint8 * data, uint length);
+uint8 find_median(uint8 data[], uint length);
 
 //given an array of a data and a length, find the mean of the array. 
-uint8 find_mean(uint8 * data, uint length);
+uint8 find_mean(uint8 data[], uint length);
 
 // given an array of a data and a length, find the maximum of the array. 
-uint8 find_maximum(uint8 * data, uint length);
+uint8 find_maximum(uint8 data[], uint length);
 
 // given an array of a data and a length, find the minimum of the array. 
-uint8 find_minimum(uint8 * data, uint length);
+uint8 find_minimum(uint8  data[], uint length);
 
 //sorts a given array of given length from largest to smallest. 
-void sort_array(uint8 * data, uint length);
+void sort_array(uint8 data[], uint length);
 
 
