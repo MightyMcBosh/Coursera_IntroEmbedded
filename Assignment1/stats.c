892a039e868bbc765c22d4223bd4cd519aeb4632
Ben Stewart <benjamin.r.stewart95@gmail.com>
2022-11-09 22:21:15 -0700

    Adding stubs for initial coding assignment

---
+++ Assignment1/stats.c
@@ -0,0 +1,79 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.c 
+ * @brief This is the code file for assignment 1. 
+ *
+ * in this assignment, we are to do several statistical analyses on the data set
+ * included in the file. 
+ *
+ * @author Benjamin Stewart
+ * @date 11/9/2022
+ *
+ */
+
+
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+ 
+
+}
+
+void print_statistics(uint8 * data, uint length)
+{
+    
+}
+
+void print_array (uint8 * data, uint length)
+{
+    
+}
+
+ 
+uint8 find_median(uint8 * data, uint length)
+{
+
+}
+
+ 
+uint8 find_mean(uint8 * data, uint length)
+{
+
+}
+
+uint8 find_maximum(uint8 * data, uint length)
+{
+
+}
+
+ 
+uint8 find_minimum(uint8 * data, uint length)
+{
+
+}
+
+void sort_array(uint8 * data, uint length)
+{
+    
+}
